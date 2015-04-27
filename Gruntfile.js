71dec8ad748c0b3132e61f5ea4eb2042acf356d8
Amit Deshmukh <amit@localhost.com>
2015-04-26 20:11:38 -0400

    added first custom task

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,3 +3,7 @@ var grunt = require('grunt');
 grunt.registerTask('default', 'default task description', function(){
   console.log('hello world');
 });
+
+grunt.registerTask('first-custom-task', 'First Custom Task', function(){
+	console.log('Running the first custom task');
+});
