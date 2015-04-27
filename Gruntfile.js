06659c42ec982546bbd2257b9535fc8d4054274b
Amit Deshmukh <amit@localhost.com>
2015-04-26 20:32:07 -0400

    addng a new task that accepts a param

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,3 +7,11 @@ grunt.registerTask('default', 'default task description', function(){
 grunt.registerTask('first-custom-task', 'First Custom Task', function(){
 	console.log('Running the first custom task');
 });
+
+grunt.registerTask('hello-name', 'say hello to name passed through command line', function(name){
+	if(!name || !name.length) {
+		grunt.warn('You need to provide a name');
+	}
+	
+	console.log('Hello ' + name);
+});
