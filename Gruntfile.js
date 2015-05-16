a4f4635dcb23991ba689aa3ce0705c4a064675e9
Amit Deshmukh <amit@localhost.com>
2015-05-16 11:21:23 -0400

    code copied from code/multiple_argument.txt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,9 @@
 var grunt = require('grunt');
 
-grunt.registerTask('default', 'default task description', function(){
-  console.log('hello world');
-});
+//ex: grunt hello-multiple-names:Amit:Jaya
+grunt.registerTask('hello-multiple-names', 'multiple args example. invoke task as hello-multiple-names:arg1:arg2', 
 
-grunt.registerTask('first-custom-task', 'First Custom Task', function(){
-	console.log('Running the first custom task');
-});
-
-grunt.registerTask('hello-name', 'say hello to name passed through command line', function(name){
-	if(!name || !name.length) {
-		grunt.warn('You need to provide a name');
-	}
-	
-	console.log('Hello ' + name);
-});
-
-grunt.registerTask('hello-multiple-names', 'multiple args example. invoke task as hello-multiple-names:arg1:arg2', function(name1, name2){
+function(name1, name2){
 	if(!name1 || !name1.length || !name2 || !name2.length) {
 		grunt.warn('Both the names are required');
 	}
