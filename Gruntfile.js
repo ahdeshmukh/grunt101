7207555f8d54c01aa9783bc468bcc36e050c5fbd
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2015-08-11 16:52:50 -0400

    code from amit-build.txt after adding processhtml

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,12 +12,63 @@ module.exports = function(grunt) {
 
 		pkg: grunt.file.readJSON('package.json'),
 		
+		less: {
+			options: {
+				banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %> \n*/\n'
+			},
+			build: {
+				files: {
+					'dist/css/style.css': 'src/**/*.less'
+				}
+			}
+		},
+		
+		cssmin: {
+			build: {
+				files: {
+					'dist/css/style.min.css': 'dist/css/style.css'
+				}
+			}
+		},
+		
+		uglify: {
+			options: {
+				banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %> \n*/\n'
+			},
+			build: {
+				files: {
+					'dist/js/all.min.js': 'src/**/*.js'
+					//'dist/js/all.min.js': ['src/**/*.js', '!src/**/filename.js'] to ignore any particular file
+				}
+			}
+		},
+		
+		copy: {
+			main: {
+				files: [
+				  {expand: true, src: ['index.html', 'copy-*/**'], dest: 'dist/'}
+				]
+			}
+		},
+
+		sync: {
+			main: {
+				files: [
+				  {src: ['index.html', 'copy-*/**'], dest: 'dist/'}
+				],
+				verbose: true,
+				failOnError: true,
+				updateAndDelete: true,
+				ignoreInDest: ['js/**', 'css/**']
+			}
+		},
+		
 		processhtml: {
 			dist: {
 	      options: {
 	        process: true,
 	        data: {
-	          message: 'This is production distribution'
+	          message: 'This is production distribution - Amit build'
 	        }
 	      },
 	      files: {
@@ -27,6 +78,8 @@ module.exports = function(grunt) {
 		}
 
 	});
+	
+	grunt.registerTask('amit-build', 'Build task', ['less', 'cssmin', 'uglify', 'copy', 'sync', 'processhtml']);
 	
 	// we can only load these if they are in our package.json
 	// make sure you have run npm install so our app can find these
